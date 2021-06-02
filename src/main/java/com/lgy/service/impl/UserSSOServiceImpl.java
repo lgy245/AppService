8fecf764be9d73ff65c5199f187a5582543bb5b2
lgy <lc0410@yeah.net>
2021-06-02 09:37:08 +0800

    集成简易redis

--- src/main/java/com/lgy/service/impl/UserSSOServiceImpl.java
+++ src/main/java/com/lgy/service/impl/UserSSOServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lgy.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.lgy.mapper.UserSSOMapper;
 import com.lgy.pojo.User;
@@ -9,8 +10,10 @@ import com.lgy.service.UserSSOService;
 import com.lgy.until.Until;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
+import java.util.Map;
 
 /*
  *@autor
@@ -21,7 +24,7 @@ import java.util.List;
 public class UserSSOServiceImpl implements UserSSOService {
     @Autowired(required = false)
     UserSSOMapper userSSOMapper;
-
+    Jedis jedis = new Jedis("192.168.18.130",6380);
     @Override
     public List<UserSSO> find(String userName, String passWorld) {
         return userSSOMapper.find(userName,passWorld);
@@ -35,6 +38,7 @@ public class UserSSOServiceImpl implements UserSSOService {
 
     @Override
     public Object findByIdOb(String userName,String passWorld) {
+
         JSONObject jsonObject = new JSONObject();
         //判断当前用户是否能登录成功
 
@@ -43,10 +47,9 @@ public class UserSSOServiceImpl implements UserSSOService {
             jsonObject.put("message","用户不存在");
             return jsonObject;
         }else{
-            System.out.println(list.get(0));
             String str = JSON.toJSONString(list.get(0));
             UserSSO userSS = JSON.parseObject(str,UserSSO.class);
-            System.out.println(userSS.getName());
+
             if(!userSS.getPassworld().equals(passWorld)){
                 jsonObject.put("message","密码错误，请重新登录");
                 return jsonObject;
@@ -56,6 +59,22 @@ public class UserSSOServiceImpl implements UserSSOService {
                 userSSO.setName(userName);
                 userSSO.setPassworld(passWorld);
                 String token = Until.getToken(userSSO);//生成token
+                 //判断 token 是否存在
+                String getToke = jedis.get(token);
+                if(getToke!="null"){
+                    String json = JSONArray.toJSON(userSSO).toString();
+                    System.out.println(json);
+                    jedis.set(token, json);
+                    System.out.println(0);
+                    String getRedisToken = jedis.get(token);
+                    System.out.println(getRedisToken);
+                    UserSSO userSSO1 = JSONArray.parseObject(json, UserSSO.class);
+                    System.out.println(userSSO1.getName());
+
+                    System.out.println(1);
+
+                }
+
                 //放入并返回
                 jsonObject.put("token",token);
                 jsonObject.put("user",list);
